ee98a50b64947483f8234717501531b9402f2e99
Yomaira Escano <yjescano@gmail.com>
2018-08-20 05:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,9 @@ class BoardMember {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+class Ceo extends BoardMember {
+  hireEmployee() {
+    return "Welcome aboard!"
+  }
+}
