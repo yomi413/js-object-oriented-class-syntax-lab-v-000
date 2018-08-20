91c7bc6d7d43b79bc5bc97a45369ab74dbdd4056
Yomaira Escano <yjescano@gmail.com>
2018-08-20 05:30:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 class BoardMember {
   constructor(name, homeState, training) {
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
+    this.name = name
+    this.homeState = homeState
+    this.training = training
   }
 
   veto() {
